bcaaec01151404de744a2f86f72cbace35eed611
Nguyễn Phan Tấn Lộc <tanloc0902790834@gmail.com>
2024-05-17 13:34:32 +0700

    Add files via upload
    gn

---
+++ src/java/ConnectDb/ConnectDB.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GIGABYTE
+ */
+package ConnectDb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Tab135
+ */
+public class ConnectDB {
+    
+    public static Connection getConnect(){
+        try{
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                return  DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=BOOKS;encrypt=true;trustServerCertificate=true","sa" ,"123");
+        }catch(Exception e){
+                return null;
+        }
+        
+
+}
+
+}
